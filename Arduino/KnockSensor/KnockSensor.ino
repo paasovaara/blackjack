3167d25b9ef7f92a34641e3fc2df92b12bd011d5
Markus Paasovaara <markus.paasovaara@futurice.com>
2017-11-11 14:00:43 +0200

    Dummy first version of KnockSensor.ino

---
+++ Arduino/KnockSensor/KnockSensor.ino
@@ -0,0 +1,19 @@
+int analogPin = 0;
+int val = 0;           // variable to store the value read
+
+void setup()
+{
+  Serial.begin(9600);              //  setup serial
+}
+
+void loop()
+{
+  val = analogRead(analogPin);     // read the input pin
+  Serial.print(0);  // To freeze the lower limit
+  Serial.print(" ");
+  Serial.print(1024);  // To freeze the upper limit
+  Serial.print(" ");
+  Serial.println(val);  // To send all three 'data' points to the plotter
+  
+  delay(10);
+}
