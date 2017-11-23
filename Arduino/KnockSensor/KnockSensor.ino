8cf650f0c8dd5ada416eb2e3f9d8968431495991
Markus Paasovaara <markus.paasovaara@futurice.com>
2017-11-23 09:58:49 +0200

    Knock sensor Arduino app, for single sensor

--- Arduino/KnockSensor/KnockSensor.ino
+++ Arduino/KnockSensor/KnockSensor.ino
@@ -1,19 +1,47 @@
 int analogPin = 0;
+int digitalOutPin = 2;
+
 int val = 0;           // variable to store the value read
+
+const int SAMPLE_RATE = 100; // Hz
+const float DELAY_MS = 1000.0f / (float)SAMPLE_RATE;
+
+const int THRESHOLD = 128; //TODO validate using debug plots
+const bool debug = false; //set true to plot all values, false for production
+
+bool highPrev = false;
 
 void setup()
 {
   Serial.begin(9600);              //  setup serial
+  pinMode(digitalOutPin, OUTPUT);
+  digitalWrite(digitalOutPin, LOW);
 }
 
 void loop()
 {
   val = analogRead(analogPin);     // read the input pin
-  Serial.print(0);  // To freeze the lower limit
-  Serial.print(" ");
-  Serial.print(1024);  // To freeze the upper limit
-  Serial.print(" ");
-  Serial.println(val);  // To send all three 'data' points to the plotter
+  if (debug) {
+    Serial.print(0);  // To freeze the lower limit
+    Serial.print(" ");
+    Serial.print(THRESHOLD);
+    Serial.print(" ");
+    Serial.print(1024);  // To freeze the upper limit
+    Serial.print(" ");
+    Serial.println(val);  // To send all the 'data' points to the plotter
+  }
+  else {
+    bool highNow = val > THRESHOLD;
+    digitalWrite(digitalOutPin, highNow ? HIGH : LOW);
+    
+    if (highNow != highPrev) {
+      Serial.println(highNow ? "KNOCK" : "NO");
+      highPrev = highNow;      
+    }
+    else {
+      //NOP
+    }
+  }
   
-  delay(10);
+  delay(DELAY_MS);
 }
