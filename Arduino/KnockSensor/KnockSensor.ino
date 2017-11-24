141f5d9b79d1244594ee6d1e6c6d8168fa0f4fb4
Markus Paasovaara <markus.paasovaara@futurice.com>
2017-11-24 14:37:52 +0200

    Revert "Read two analog inputs at the same time. revert this if you want to read only one, there might be interference"
    This reverts commit f6bc97838342de3e34549989323f43fe9b33011e.

--- Arduino/KnockSensor/KnockSensor.ino
+++ Arduino/KnockSensor/KnockSensor.ino
@@ -1,41 +1,26 @@
-int analogPin1 = 0;
-int digitalOutPin1 = 2;
-int analogPin2 = 1;
-int digitalOutPin2 = 6;
+int analogPin = 0;
+int digitalOutPin = 2;
 
-//TODO test, possibly increase sample freq to 200?
+int val = 0;           // variable to store the value read
+
 const int SAMPLE_RATE = 100; // Hz
 const float DELAY_MS = 1000.0f / (float)SAMPLE_RATE;
 
 const int THRESHOLD = 128; //TODO validate using debug plots
-const bool debug = true; //set true to plot all values, false for production
+const bool debug = false; //set true to plot all values, false for production
 
-//Object oriented way would be nicer.. TODO this in C++
-bool highPrev1 = false;
-bool highPrev2 = false;
-//int val1 = 0;
+bool highPrev = false;
 
 void setup()
 {
   Serial.begin(9600);              //  setup serial
-  pinMode(digitalOutPin1, OUTPUT);
-  digitalWrite(digitalOutPin1, LOW);
-  
-  pinMode(digitalOutPin2, OUTPUT);
-  digitalWrite(digitalOutPin2, LOW);
+  pinMode(digitalOutPin, OUTPUT);
+  digitalWrite(digitalOutPin, LOW);
 }
 
 void loop()
 {
-  // read the input pins
-  int val1 = analogRead(analogPin1); 
-  //val1 = analogRead(analogPin1); 
-  // Some cool-down time for ADC might be needed
-  // https://github.com/firmata/arduino/issues/334
-  delay(DELAY_MS / 2); 
-  int val2 = analogRead(analogPin2);
-  //val2 = analogRead(analogPin2);
-  
+  val = analogRead(analogPin);     // read the input pin
   if (debug) {
     Serial.print(0);  // To freeze the lower limit
     Serial.print(" ");
@@ -43,35 +28,22 @@ void loop()
     Serial.print(" ");
     Serial.print(1024);  // To freeze the upper limit
     Serial.print(" ");
-    Serial.print(val1);
-    Serial.print(" ");
-    Serial.println(val2);  // To send all the 'data' points to the plotter
+    Serial.println(val);  // To send all the 'data' points to the plotter
   }
   else {
-    /// Sensor 1
-    bool highNow1 = val1 > THRESHOLD;
-    digitalWrite(digitalOutPin1, highNow1 ? HIGH : LOW);
+    bool highNow = val > THRESHOLD;
+    digitalWrite(digitalOutPin, highNow ? HIGH : LOW);
     
-    if (highNow1 != highPrev1) {
-      highPrev1 = highNow1;
-      if (highNow1) {
-        Serial.println("KNOCK1");
+    if (highNow != highPrev) {
+      highPrev = highNow;
+      if (highNow) {
+        Serial.println("KNOCK");
       }
     }
-
-    /// Sensor 2
-    bool highNow2 = val2 > THRESHOLD;
-    digitalWrite(digitalOutPin2, highNow2 ? HIGH : LOW);
-    
-    if (highNow2 != highPrev2) {
-      highPrev2 = highNow2;
-      if (highNow2) {
-        Serial.println("KNOCK2");
-      }
+    else {
+      //NOP
     }
   }
   
-  //delay(DELAY_MS);
-  delay(DELAY_MS / 2); 
-  
+  delay(DELAY_MS);
 }
