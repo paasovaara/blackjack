803659aa232e06bbe3a1b7e71f655b9af9cfc88d
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-01-13 14:55:40 +0200

    Fix the plot

--- Octave/plotDecisionBoundary.m
+++ Octave/plotDecisionBoundary.m
@@ -23,8 +23,8 @@ if size(X, 2) <= 3
     plot(plot_x, plot_y)
     
     % Legend, specific for the exercise
-    legend('Admitted', 'Not admitted', 'Decision Boundary')
-    axis([30, 100, 30, 100])
+    legend('Hit', 'Stay', 'Decision Boundary')
+    %axis([30, 100, 30, 100])
 else
     % Here is the grid range
     u = linspace(-1, 1.5, 50);
