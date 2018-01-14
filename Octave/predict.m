5ab9e23bd25457724e2d28a794d7517e1d5ab08e
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-01-14 09:59:48 +0200

    Cleaned up Octave scripts

--- Octave/predict.m
+++ Octave/predict.m
@@ -6,21 +6,10 @@ function p = predict(theta, X)
 
 m = size(X, 1); % Number of training examples
 
-% You need to return the following variables correctly
 p = zeros(m, 1);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters. 
-%               You should set p to a vector of 0's and 1's
-%
-
 
 probabilities = sigmoid(X * theta); % X is mxn, theta nx1, 
 
 p = probabilities >= 0.5;
-
-% =========================================================================
-
 
 end
